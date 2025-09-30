0e921317018478ffb4d2be221ba6fa5e8813ed4e
talk2klyro <talk2klyro@gmail.com>
2025-09-30 08:02:15 +0100

    Create AppNavigator.js

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+impoimport React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import SignupScreen from "../screens/SignupScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        {/* Later we’ll add Home, Tribe Chat, Announcements */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
